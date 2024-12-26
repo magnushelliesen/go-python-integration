cd1941aa3eea143a601df61764d3c538872b6b58
magnushelliesen <magnus.helliesen@gmail.com>
2024-12-26 11:15:55 +0100

    Changed function to return a number

--- src/library.go
+++ src/library.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 )
 
-//export helloWorld
-func helloWorld() {
-	fmt.Print("Hello World")
+//export print_a_number
+func print_a_number(num int) {
+	fmt.Print("The number is ", num)
 }
 
 func main() {
