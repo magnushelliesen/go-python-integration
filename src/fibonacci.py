331a3e3b5d51614cdf154a616de256cfea5debfa
magnushelliesen <magnus.helliesen@gmail.com>
2024-12-27 10:42:56 +0100

    Restructured

---
+++ src/fibonacci.py
@@ -0,0 +1,10 @@
+import ctypes
+
+library = ctypes.cdll.LoadLibrary('src/library.so')
+fibonacci_iterative = library.fibonacci_iterative
+
+fibonacci_iterative.argtypes = [ctypes.c_int]
+fibonacci_iterative.restype = ctypes.c_char_p
+
+def fibonacci(num):
+    return int(fibonacci_iterative(num).decode())
