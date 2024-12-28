f90fa8d86db78789dc5347b54fe4953a0738665f
magnushelliesen <magnus.helliesen@gmail.com>
2024-12-28 10:21:41 +0100

    Added inefficient recusive function

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -3,9 +3,13 @@ from pathlib import Path
 
 library = ctypes.cdll.LoadLibrary(Path(__file__).resolve().parent / "go/library.so")
 fibonacci_iterative = library.fibonacci_iterative
+fibonacci_recursive = library.fibonacci_recursive
 
 fibonacci_iterative.argtypes = [ctypes.c_int]
 fibonacci_iterative.restype = ctypes.c_char_p
 
-def fibonacci(num):
-    return int(fibonacci_iterative(num).decode())
+def fibonacci(num, recursive=False):
+    if recursive is False:
+        return int(fibonacci_iterative(num).decode())
+    else:
+        return fibonacci_recursive(num)
